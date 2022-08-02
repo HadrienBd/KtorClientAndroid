c2d9f66a1a090467d702bf0fb21630397ff34062
HadrienBd <hadrien.bernard@worldline.com>
2022-08-02 17:45:54 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/ktorclientandroid/data/remote/PostsService.kt
@@ -0,0 +1,39 @@
+package com.plcoding.ktorclientandroid.data.remote
+
+import com.plcoding.ktorclientandroid.data.remote.dto.PostRequest
+import com.plcoding.ktorclientandroid.data.remote.dto.PostResponse
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+interface PostsService {
+
+    suspend fun getPosts(): List<PostResponse>
+
+    suspend fun getPostById(id: Long) : PostResponse?
+
+    suspend fun createPost(postRequest: PostRequest): PostResponse?
+
+    companion object {
+        fun create(): PostsService {
+            return PostsServiceImpl(
+                client = HttpClient(Android) {
+                    install(Logging) {
+                        level = LogLevel.ALL
+                    }
+                    install(ContentNegotiation) {
+                        json(Json {
+                            prettyPrint = true
+                            ignoreUnknownKeys = true
+                            coerceInputValues = true
+                        })
+                    }
+                }
+            )
+        }
+    }
+}
+
---
+++ app/src/main/java/com/plcoding/ktorclientandroid/data/remote/PostsServiceImpl.kt
@@ -0,0 +1,51 @@
+package com.plcoding.ktorclientandroid.data.remote
+
+import com.plcoding.ktorclientandroid.data.remote.dto.PostRequest
+import com.plcoding.ktorclientandroid.data.remote.dto.PostResponse
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+
+class PostsServiceImpl(
+    private val client: HttpClient
+) : PostsService {
+
+    override suspend fun getPosts(): List<PostResponse> {
+        return try {
+            client.get(HttpRoutes.POSTS).body()
+        } catch(e: ClientRequestException) { // 4xx - responses
+            println("Error: ${e.response.status.description}")
+            emptyList()
+        } catch(e: ServerResponseException) { // 5xx - responses
+            println("Error: ${e.response.status.description}")
+            emptyList()
+        } catch(e: Exception) {
+            println("Error: ${e.message}")
+            emptyList()
+        }
+    }
+
+    override suspend fun getPostById(id: Long): PostResponse? {
+        return try {
+            client.get("${HttpRoutes.POSTS}/$id").body()
+        } catch(e: Exception) {
+            null
+        }
+    }
+
+    override suspend fun createPost(postRequest: PostRequest): PostResponse? {
+        return try {
+            client.post(HttpRoutes.POSTS) {
+                contentType(ContentType.Application.Json)
+                setBody(postRequest)
+            }.body()
+        } catch(e: Exception) {
+            println("Error: ${e.message}")
+            null
+        }
+    }
+}
+
+
